8bb35a9540606060942f12e6af44949c93db84ed
Patricia Frogoso <pfrogoso@gmail.com>
2023-10-26 12:53:11 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+const cards = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(cards) {
+    const messages = []
+    for (let i = 0; i < cards.length; i++) {
+      console.log(`Thank you, ${cards[i]}, for the wonderful surprise gift!`);
+      messages.push (`Thank you, ${cards[i]}, for the wonderful surprise gift!`)
+      debugger;
+    }
+  
+    return messages;
+  }
+
+  writeCards (cards);
+  
+  function countDown (cards) {
+    let countDown = 0;
+    while (countDown < 11) {
+      console.log(countDown++);
+    }
+
+    return cards;
+  }
